95798cc243cfc7ec5e6534f46dc49049e3f74819
Carmen Wick <CarmenWick@gmail.com>
2012-11-21 17:32:12 -0800

    Initial commit.

---
+++ mtg.js
@@ -0,0 +1,160 @@
+/* Number of different ways to form a set of k items
+ * from another set of n items, without replacement. The
+ * same items arranged in a different order are not
+ * counted as a new permutation.
+*/
+function binomial(n, k) {
+  if (k > n)
+    return 0;
+
+  bc = new Array();
+  for (var i=0; i<k+1; i++) {
+    bc[i] = 1;
+  }
+
+  for (var j=1; j<n-k+1; j++) {
+    for (var i=1; i<k+1; i++) {
+      bc[i] = bc[i-1]+bc[i];
+    }
+  }
+
+  return bc[k];
+}
+
+/**
+ * Calculates probability of drawing a given card exactly X times,
+ * where that card has 'num_copies' copies in the deck.
+ */
+function drawCard(deck_size, hand_size, num_copies, x) {
+  // Cards remaining in the deck that are not a copy of our desired card.
+  var remaining_cards = deck_size - num_copies;
+
+  return binomial(num_copies, x) * (binomial(remaining_cards, hand_size - x) / binomial(deck_size, hand_size));
+}
+
+/** Calculates probability of drawing a given card at least x times,
+ * where that card has 'num_copies' copies in the deck.
+ */
+function drawAtLeast(deck_size, hand_size, num_copies, x) {
+  var p = 0;
+  var max_times = Math.min(num_copies, hand_size);
+
+  for (var i=x ; i<=max_times ; i++) {
+    p += drawCard(deck_size, hand_size, num_copies, i);
+  }
+
+  return p;
+}
+
+/* Computes all the different ways
+ * that objects from 'items' can be grouped
+ * into 'k' buckets. 'restrictions' indicates
+ * the minimum number of items of a particular
+ * type that must be used in the grouping.
+ * For example:
+ * There are 15 objects of type A, 10 of type B, and
+ * 2 of type C.
+ * items = [15, 10, 2]
+ *
+ * There must be at least 2 type A objects, at least
+ * one type B object, and at least one type C object.
+ * restrictions = [2, 1, 1]
+ *
+ * We have 5 buckets in which to group all the objects.
+ * k = 5.
+ *
+ * The output will be an array of arrays:
+ * [[2, 1, 2], [2, 2, 1], [3, 1, 1]]
+ *
+ * Each element in each subarray indicates how
+ * many slots an object of that type should occupy.
+ * For example, [2, 1, 2] means "two slots are
+ * filled by type A, one slot is filled by type B,
+ * and two slots are filled by type C". Notice that
+ * the sum of all items in each subarray is equal
+ * to k.
+ */
+function composition(items, restrictions, k) {
+  var counter = new Array(items.length);
+  for (var i=0; i<counter.length; i++) {
+    counter[i] = 0;
+  }
+
+  return composition2(items, restrictions, counter, 0, k, []);
+}
+
+function composition2(items, restrictions, counter, i, k, result) {
+  if (i >= counter.length) {
+    var sum = 0;
+    counter.forEach(function(x) { sum += x; });
+
+    if (sum == k) {
+      result.push(counter.slice(0));
+    }
+
+    return;
+  }
+
+  for (var x=restrictions[i]; x <= Math.min(k,items[i]); x++) {
+    counter[i] = x;
+    composition2(items, restrictions, counter, i+1, k, result);
+  }
+
+  return result;
+}
+
+/*
+ * Calculates the probability of certain cards appearing in
+ * a hand of size 'hand_size'.
+ * 'cards' is an array of numbers representing quantities of
+ * different card types.
+ * 'restrictions' is an array representing the minimum number
+ * of each type of card that must appear in the hand.
+ * 'hand_size' is the number of cards in the hand.
+ *
+ * Example:
+ * We have 25 mountains, 4 Searing Spears (converted mana cost of 2),
+ * and 60-25-4 = 31 other cards. What is the probability
+ * of having at least one Searing Spear, plus at least two
+ * mountains (needed to cast Searing Spear) in your opening hand?
+ *
+ * 25 Mountains, 4 Searing Spears, 31 other cards
+ * cards = [25, 4, 31]
+ *
+ * We need at least two mountains, at least one searing spear,
+ * and zero or more other cards. i.e. We allow hands that contain
+ * 5 mountains and two Searing Spears.
+ * restrictions = [2, 1, 0]
+ *
+ * Starting hands have 7 cards.
+ * hand_size = 7
+ *
+ * Note that although 'restrictions' only specify minimum quantities,
+ * you can actually indirectly specify maximum quantities as well. For
+ * example, if we wanted our opening hand to contain exactly two mountains
+ * and exactly one searing spear, we could set 'restrictions' to [2,1,4],
+ * thus ensuring that "other cards" (which aren't a mountain or searing spear),
+ * occupy at least 4 slots.
+ */
+function hand_probability(cards, restrictions, hand_size) {
+  // Need to change this if arguments come in as a
+  // vertical, as opposed to horizontal, range from
+  // the spreadsheet.
+  cards = cards[0];
+  restrictions = restrictions[0];
+
+  var ksum = 0;
+  composition(cards, restrictions, hand_size).forEach(function(c) {
+    var product = 1;
+    c.forEach(function(k, idx) {
+      product *= binomial(cards[idx], k);
+    });
+    ksum += product;
+  });
+
+  var deck_size = 0;
+  cards.forEach(function(x) { deck_size += x; });
+
+  return ksum / binomial(deck_size, hand_size);
+}
+
